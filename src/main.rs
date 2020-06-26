212c0083f8c37e215900209cb5b396b9d553602e
Tatsuki Saito <cc_called_pizza@yahoo.co.jp>
2020-06-26 15:19:16 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -41,10 +41,55 @@ fn a04() {
     println!("{}", &res[1..]);
 }
 
+fn a05a(s: String, n: usize) -> Vec<String> {
+    let mut v = vec![];
+    // vec![String::from("hoge")];
+    for i in 0..(s.len() - n + 1) {
+        v.push(String::from(&s[i..(i+n)]));
+    }
+    return v;
+}
+
+
+fn a05b(s: &str, n: usize) -> Vec<Vec<&str>> {
+    let v: Vec<&str> = s.split_whitespace().collect();
+    let mut res = vec![];
+    for i in 0..(v.len() - n + 1) {
+        res.push(Vec::from(&v[i..(i+n)]));
+    }
+    return res;
+}
+
+fn a06() {
+    let sample1 = "paraparaparadise";
+    let sample2 = "paragraph";
+    let mut st1: HashSet<&str> = HashSet::new();
+    let mut st2: HashSet<&str> = HashSet::new();
+    for i in 0..(sample1.len() - 1) {
+        st1.insert(&sample1[i..(i+2)]);
+    }
+    for i in 0..(sample2.len() - 1) {
+        st2.insert(&sample2[i..(i+2)]);
+    }
+    let unin: Vec<&str> = st1.union(&st2).map(|s| *s).collect();
+    let intr: Vec<&str> = st1.intersection(&st2).map(|s| *s).collect();
+    let diff: Vec<&str> = st1.difference(&st2).map(|s| *s).collect();
+    println!("{:?}", unin);
+    println!("{:?}", intr);
+    println!("{:?}", diff);
+}
+
 fn main() {
     a00();
     a01();
     a02();
     a03();
     a04();
+    println!("{:?}", a05a(String::from("I am an NLPer"), 2));
+    println!("{:?}", a05a(String::from("I am an NLPer"), 3));
+    println!("{:?}", a05a(String::from("I am an NLPer"), 4));
+    println!("{:?}", a05b("I am an NLPer", 2));
+    println!("{:?}", a05b("I am an NLPer", 3));
+    println!("{:?}", a05b("I am an NLPer", 4));
+    a06();
 }
